d40e196c9d01a34c7af4663b90a4aab2421fa6e2
gomezrondon <gomezrondon@gmail.com>
2020-10-27 14:07:40 -0600

    anotating a endpoint.

---
+++ Controllers/CommandsController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Commander.Controllers
+{
+
+    //api/commands
+    // [Route("api[controller]")] 
+    [Route("api/commands")]
+    [ApiController]
+    public class CommandsController : ControllerBase
+    {
+        
+    }
+}
