6d8e674b87c8d4242d87cb20f4064c5c5c96a65d
gomezrondon <gomezrondon@gmail.com>
2020-10-27 15:11:59 -0600

    Adding connection to mongo db.

---
+++ Data/MongoDBRespository.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using MongoDB.Driver;
+
+namespace Commander.Data
+{
+    public class MongoDBRespository
+    {
+        public MongoClient client;
+
+        public IMongoDatabase db;
+
+        public MongoDBRespository()
+        {
+            //
+            client = new MongoClient("mongodb+srv://<user>:<password>@<cluster>/<dbname>?retryWrites=true&w=majority");
+            db = client.GetDatabase("dbname");
+        }
+    }
+}
