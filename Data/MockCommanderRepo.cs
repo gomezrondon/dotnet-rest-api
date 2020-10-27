f70d910eb2ff513d5273a47c6f436c7fa6aadd74
gomezrondon <gomezrondon@gmail.com>
2020-10-27 14:07:50 -0600

    creating mock data.

---
+++ Data/MockCommanderRepo.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Commander.Models;
+
+namespace Commander.Data
+{
+    public class MockCommanderRepo : ICommanderRepo
+    {
+        public IEnumerable<Command> GetAppCommands()
+        {
+            var commands = new List<Command> {
+                new Command { Id = 0, HowTo = "dotnet new", Line = "dotnet new", Platform = ".Net" },
+            new Command { Id = 1, HowTo = "dotnet new webapi -n <app name>", Line = "dotnet new webapi -n <app name>", Platform = ".Net" },
+            new Command { Id = 2, HowTo = "dotnet new gitignore", Line = "dotnet new gitignore", Platform = ".Net" }
+        };
+
+            return commands;
+
+        }
+
+        public Command GetCommandById(int id)
+        {
+            return new Command { Id = 0, HowTo = "dotnet new", Line = "dotnet new", Platform = ".Net" };
+        }
+    }
+}
